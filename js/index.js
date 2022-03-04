42179cef4ccf34f67a9af2d2de79d70b4e9960e7
dement6d <cuff.ho@gmail.com>
2022-03-04 22:25:34 +0100

    make addresses copyable

---
+++ js/index.js
@@ -0,0 +1,15 @@
+function copyXmrDemented() {
+    var r = document.createRange();
+    r.selectNode(document.getElementById("xmrDemented"));
+    window.getSelection().removeAllRanges();
+    window.getSelection().addRange(r);
+    document.execCommand('copy');
+    window.getSelection().removeAllRanges();
+function copyBtcDemented() {
+    var r = document.createRange();
+    r.selectNode(document.getElementById("btcDemented"));
+    window.getSelection().removeAllRanges();
+    window.getSelection().addRange(r);
+    document.execCommand('copy');
+    window.getSelection().removeAllRanges();
+}
