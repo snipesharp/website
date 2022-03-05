193d599a978885397186ae109b17b29fbcd06d4e
dement6d <cuff.ho@gmail.com>
2022-03-05 15:42:56 +0100

    stiliyan address.

--- js/index.js
+++ js/index.js
@@ -14,3 +14,11 @@ function copyBtcDemented() {
     document.execCommand('copy');
     window.getSelection().removeAllRanges();
 }
+function copyXmrStiliyan() {
+    var r = document.createRange();
+    r.selectNode(document.getElementById("xmrStiliyan"));
+    window.getSelection().removeAllRanges();
+    window.getSelection().addRange(r);
+    document.execCommand('copy');
+    window.getSelection().removeAllRanges();
+}
