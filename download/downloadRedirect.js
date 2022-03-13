b71cf8e4146aa20a903f18f8a6692fdbe0001955
dement6d <cuff.ho@gmail.com>
2022-03-13 16:04:05 +0100

    add download dir

---
+++ download/downloadRedirect.js
@@ -0,0 +1,18 @@
+let os = getOS()
+let downloadText = document.getElementById("downloadText")
+
+if (os == "None") {
+    location.replace("/")
+    throw new Error("Non compatible user agent")
+}
+
+downloadText.textContent = "Downloading snipesharp for " + os;
+
+if (os == "Linux") location.replace("https://github.com/snipesharp/snipesharp/releases/download/v1.5.7/snipesharp_linux-x86-64-v1.5.7")
+if (os == "Linux ARM64") location.replace("https://github.com/snipesharp/snipesharp/releases/download/v1.5.7/snipesharp_linux-arm64-v1.5.7")
+
+if (os == "Windows") location.replace("https://github.com/snipesharp/snipesharp/releases/download/v1.5.7/snipesharp_win-x86-64-v1.5.7.exe")
+if (os == "Windows ARM64") location.replace("https://github.com/snipesharp/snipesharp/releases/download/v1.5.7/snipesharp_win-arm64-v1.5.7.exe")
+
+if (os == "MacOS") location.replace("https://github.com/snipesharp/snipesharp/releases/download/v1.5.7/snipesharp_mac-os-x86-64-v1.5.7")
+if (os == "MacOS ARM64") location.replace("https://github.com/snipesharp/snipesharp/releases/download/v1.5.7/snipesharp_mac-os-arm64-v1.5.7")
