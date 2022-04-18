b133349abbaee454a27c87c8c48e4b3d26a83651
dement6d <cuff.ho@gmail.com>
2022-04-19 01:35:20 +0200

    update to 1.7.5

--- js/userAgentDownloads.js
+++ js/userAgentDownloads.js
@@ -51,12 +51,12 @@ function toggleShowAllOptions(event) {
     downloadLink.textContent = "Download for " + os
     downloadLink.classList.add("downloadButton")
 
-    if (os == "Linux") downloadLink.setAttribute("href", "https://github.com/snipesharp/snipesharp/releases/download/v1.7.4/snipesharp_linux-x86-64-v1.7.4");
-    if (os == "Linux ARM64") downloadLink.setAttribute("href", "https://github.com/snipesharp/snipesharp/releases/download/v1.7.4/snipesharp_linux-arm64-v1.7.4");
+    if (os == "Linux") downloadLink.setAttribute("href", "https://github.com/snipesharp/snipesharp/releases/download/v1.7.5/snipesharp_linux-x86-64-v1.7.5");
+    if (os == "Linux ARM64") downloadLink.setAttribute("href", "https://github.com/snipesharp/snipesharp/releases/download/v1.7.5/snipesharp_linux-arm64-v1.7.5");
 
-    if (os == "Windows") downloadLink.setAttribute("href", "https://github.com/snipesharp/snipesharp/releases/download/v1.7.4/snipesharp_win-x86-64-v1.7.4.exe");
-    if (os == "Windows ARM64") downloadLink.setAttribute("href", "https://github.com/snipesharp/snipesharp/releases/download/v1.7.4/snipesharp_win-arm64-v1.7.4.exe");
+    if (os == "Windows") downloadLink.setAttribute("href", "https://github.com/snipesharp/snipesharp/releases/download/v1.7.5/snipesharp_win-x86-64-v1.7.5.exe");
+    if (os == "Windows ARM64") downloadLink.setAttribute("href", "https://github.com/snipesharp/snipesharp/releases/download/v1.7.5/snipesharp_win-arm64-v1.7.5.exe");
 
-    if (os == "MacOS") downloadLink.setAttribute("href", "https://github.com/snipesharp/snipesharp/releases/download/v1.7.4/snipesharp_mac-os-x86-64-v1.7.4");
-    if (os == "MacOS ARM64") downloadLink.setAttribute("href", "https://github.com/snipesharp/snipesharp/releases/download/v1.7.4/snipesharp_mac-os-arm64-v1.7.4");
+    if (os == "MacOS") downloadLink.setAttribute("href", "https://github.com/snipesharp/snipesharp/releases/download/v1.7.5/snipesharp_mac-os-x86-64-v1.7.5");
+    if (os == "MacOS ARM64") downloadLink.setAttribute("href", "https://github.com/snipesharp/snipesharp/releases/download/v1.7.5/snipesharp_mac-os-arm64-v1.7.5");
 }())
